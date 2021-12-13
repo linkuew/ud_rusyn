607728c1e427b4eaef1b442ff8fd29c695881a5a
Matthew <37814869+linkuew@users.noreply.github.com>
2021-12-12 21:24:55 -0500

    Annotated first batch of sentences, added two models, first start at making mixed lang parser

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # Universal Dependency treebank for Rusyn
 A database for the beginning of a UD treebank for the Rusyn language
 
+# Set up
+To better facilitate an agreeable computing environment, here are the
+things I've used to run this code and ensure smooth optimal operations.
+
+*Creating an R environment*
+Rather than attempt to reinvent multiple wheels, the carriage, and the horse,
+I've chosen to utilize the framework Miniconda. This can be downloaded (as of
+19/10/2021) at [https://docs.conda.io/en/latest/miniconda.html].
+
+Once you've installed Miniconda, you can now follow the instructions here
+(19/10/2021) [https://docs.anaconda.com/anaconda/user-guide/tasks/using-r-language/]
+
+Lastly, you'll need the R package udpipe, so you can run:
+conda install -n {r-env-name} r-udpipe
+
+*Info about my environment*
+This code is running in Rscript version 3.6.1 within conda 4.10.3.
+
+
 # Acknowledgements
 This work has been generated using multiple resources
 
@@ -8,3 +27,5 @@ Wikipedia Data: © 2021 Abteilung Automatische Sprachverarbeitung, Universität 
 (their usage terms found here: https://wortschatz.uni-leipzig.de/en/usage)
 
 UD Pipe: Straka, Milan and Straková, Jana, 2019, Universal Dependencies 2.5 Models for UDPipe (2019-12-06), LINDAT/CLARIAH-CZ digital library at the Institute of Formal and Applied Linguistics (ÚFAL), Faculty of Mathematics and Physics, Charles University, http://hdl.handle.net/11234/1-3131.
+
+Anaconda: (https://www.anaconda.com/)
--- scripts_R/read_annotate.R
+++ scripts_R/read_annotate.R
@@ -22,15 +22,19 @@ print_help <- function()
 {
         cat("Usage: Rscript read_annotate.R arg1 arg2\n")
         cat("arg1: /path/to/textfile - this assumes that each
-              sentence is on a separate line\n")
+            sentence is on a separate line\n")
         cat("arg2: /path/to/UDPipemodel - should probably be
-              in the similar language family tree as the text file language\n")
+                in the similar language family tree as the text file language\n")
+        #TODO
+        #cat("arg3: num_of_annos - number of annotations you'd like to perform,
+        #        starting from the beginning of the file arg1\n")
 }
 
 # read through text file and parse sentence into CoNLL-U format
 read_anno <- function(text_file, ud_file)
 {
         library(udpipe)
+        library(stringr)
 
         udmodel = udpipe_load_model(file = ud_file)
         text_file = file(text_file, "r")
@@ -38,16 +42,19 @@ read_anno <- function(text_file, ud_file)
         # specify the number of lines you'd like to read from
         # the beginning of the file by changing the number of
         # iterations here
-        #for (i in 1:100) {
+        # TODO
+        #for (i in 1:args[8]) {
         while (TRUE) {
                 line = readLines(text_file, n = 1)
                 if (length(line) == 0) {
                         break
                 }
+                line = str_split(line, '\t', n = 2)[[1]][2]
                 line_anno <- udpipe(x = line, object = udmodel,
-                                tagger = c("default"),
-                                parser = c("none"))
+                                    tagger = c("default"),
+                                    parser = c("none"))
                 cat(as_conllu(line_anno))
+                # extra stuff you may want to use
                 #line_anno = as.data.frame(line_anno)
                 #table(line_anno$conllu)
                 #str(line_anno)
@@ -59,7 +66,10 @@ read_anno <- function(text_file, ud_file)
 main <- function()
 {
         args <- commandArgs()
-        if (length(args) != 7) {
+
+        #TODO
+        # if(length(args) != 8) {
+        if (length(args) < 7) {
                 print_help()
                 quit(status = 1)
         }
